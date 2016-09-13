112866e2702ae5098fac49a144557dd85db711f0
Bruno@Madrid <brunosan@gmail.com>
2016-09-13 18:10:52 -0400

    use declared l2

--- cnn-text-classification-tf/text_cnn.py
+++ cnn-text-classification-tf/text_cnn.py
@@ -9,7 +9,7 @@ class TextCNN(object):
     """
     def __init__(
       self, sequence_length, num_classes, vocab_size,
-      embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+      embedding_size, filter_sizes, num_filters, l2_reg_lambda):
 
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
