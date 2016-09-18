44d4733929326c4ca5db2e5e142f7f70d45bcc3b
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2016-09-18 14:10:18 -0400

    Ready for #1 runs

--- cnn-text-classification-tf/data_helpers.py
+++ cnn-text-classification-tf/data_helpers.py
@@ -31,9 +31,9 @@ def load_data_and_labels():
     Returns split sentences and labels.
     """
     # Load data from files
-    positive_examples = list(open("./data/rt-polaritydata/rt-polarity.pos", "r").readlines())
+    positive_examples = list(open("../data/positive_ratings.csv", "r").readlines())
     positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = list(open("./data/rt-polaritydata/rt-polarity.neg", "r").readlines())
+    negative_examples = list(open("../data/negative_ratings.csv", "r").readlines())
     negative_examples = [s.strip() for s in negative_examples]
     # Split by words
     x_text = positive_examples + negative_examples
